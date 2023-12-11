97c4e84ce8028aef407798a969954bf300ebc336
Balaji <lokesh111193@gmail.com>
2023-12-11 18:31:05 +0530

    TC_ooq Branch Committed

--- src/test/java/bhuvanesh/bgrfv.java
+++ src/test/java/bhuvanesh/bgrfv.java
@@ -3,5 +3,8 @@ package bhuvanesh;
 public class bgrfv {
 public static void main(String[] args) {
 	System.out.println("y");
+	System.out.println("y1");
+	System.out.println("y2-tc_001");
+	
 }
-}
+} 
